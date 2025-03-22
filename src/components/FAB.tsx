b3d832567c7441b6f1b27dedb0b794d7b18e8608
sai shravan <saishravan384@gmail.com>
2025-03-22 13:55:41 +0530

    feat: Add Floating Action Button (FAB) component with tooltip and customizable theme color

---
+++ src/components/FAB.tsx
@@ -0,0 +1,30 @@
+import { plusIcon } from '@progress/kendo-svg-icons'
+import {
+  FloatingActionButton,
+  FloatingActionButtonThemeColor,
+} from '@progress/kendo-react-buttons'
+import { Tooltip } from '@progress/kendo-react-tooltip'
+import { FC } from 'react'
+
+interface FABProps {
+  themeColor: FloatingActionButtonThemeColor | undefined
+  tooltip: string
+  testId?: string
+}
+
+const FAB: FC<FABProps> = ({ themeColor, tooltip, testId = '' }: FABProps) => {
+  return (
+    <Tooltip anchorElement='target' position='auto'>
+      <FloatingActionButton
+        themeColor={themeColor}
+        svgIcon={plusIcon}
+        alignOffset={{ x: 20, y: 70 }}
+        className='static-fab'
+        title={tooltip}
+        data-testid={testId}
+      />
+    </Tooltip>
+  )
+}
+
+export default FAB
